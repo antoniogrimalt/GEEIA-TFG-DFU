f8f44b47fe64a67bb10f3757243be8adaa891c6a
antoniogrimalt <antoniogrimalt@gmail.com>
2024-08-22 18:49:25 +0200

    Add ros_comm repo

--- cv_bridge/setup.py
+++ cv_bridge/setup.py
@@ -4,8 +4,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
 
-d['packages'] = ['cv_bridge', 'cv_bridge.boost']
-d['package_dir'] = {'' : 'python'}
-d['version'] += ".post1"
+d["packages"] = ["cv_bridge", "cv_bridge.boost"]
+d["package_dir"] = {"": "python"}
+d["version"] += ".post1"
 
 setup(**d)
